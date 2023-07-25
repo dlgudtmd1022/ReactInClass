448da8d664fff3081ef4945de89637e60f1eb609
이형승 <dlgudtmd1022@gamil.com>
2023-07-25 14:41:29 +0900

    react day94

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
-import MyComponent from "./MyComponent";
-import YourComponent from "./YourComponent";
-import Say from "./Say";
+import MyComponent from "./components/study/MyComponent";
+import YourComponent from "./components/study/YourComponent";
+import Say from "./components/study/Say";
+import ArrayChange from "./components/study/ArrayChange";
+import EventPractice from "./components/eventhandling/EventPractice";
 
 function App() {
   return (
       <>
           {/* propertie명은 자유롭게 지정해주시면 됩니다.
            다만 받는 쪽에서도 전달해줄때 사용해준 명칭을 사용해야 연결됩니다.*/}
-        <MyComponent>사이에 적은 내용</MyComponent>
-        <YourComponent country="ROK"/>
-        <Say msg="안녕하세요"/>
+        {/*<MyComponent>사이에 적은 내용</MyComponent>*/}
+        {/*<YourComponent country="ROK"/>*/}
+        {/*<Say msg="안녕하세요"/>*/}
+        {/*<ArrayChange/>*/}
+        <EventPractice/>
       </>
   );
 }
